f4beb636037811396c3241a2b02b10a53a23b3ae
nemaICT-softwareDev <neminha13@gmail.com>
2023-05-09 13:50:50 +0200

    updated files

--- src/main/java/fullstacktraining/springboot/react/controller/GameController.java
+++ src/main/java/fullstacktraining/springboot/react/controller/GameController.java
@@ -1,4 +1,6 @@
 package fullstacktraining.springboot.react.controller;
+
+
 import fullstacktraining.springboot.react.model.Game;
 import fullstacktraining.springboot.react.service.GameService;
 import fullstacktraining.springboot.react.utils.Utils;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/games")
